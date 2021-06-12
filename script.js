965dbe335fd801bd38d1eae9b5fc664447f68d56
rkdudley <rattikarndudley@gmail.com>
2021-06-11 21:55:12 -0400

    fix bug from null clicking element

--- script.js
+++ script.js
@@ -6,74 +6,57 @@ class List {
     ul = document.querySelector('#addList');
     items = [];
 
-    //not sure why i cannot grab elements of span text in li
-    bin = document.querySelector('span.text');
-
     addItems = function () {
         this.add.addEventListener('click', (e) => {
-            console.log(this.bin);
             e.preventDefault();
+
+            let delBtn = document.createElement('button');
+            let checkBtn = document.createElement('button');
+            let li = document.createElement('li');
+
             if (this.list.value === '') {
                 this.warning.innerHTML = "Please enter your to do list";
                 this.warning.style.color = "red";
             } else {
                 this.warning.innerHTML = "";
-                this.items.push( this.list.value);
+                this.items.push(this.list.value);
 
-                //create the li element
-                let li = document.createElement('li');
                 li.className = "details"
                 li.classList.add('fadeInOpacity');
-                li.innerHTML = `<span class="text">${this.items[this.items.length-1]} </span>`;
+                delBtn.innerHTML = `<i class="far fa-trash-alt bin"></i>`;
+                checkBtn.innerHTML = `<i class="far fa-check-circle"></i>`;
+                li.innerHTML = `<span class="text">${this.items[this.items.length - 1]} </span>`;
 
-                //append the list
+                //append the text and icons
                 this.ul.appendChild(li);
+                li.appendChild(delBtn);
+                li.appendChild(checkBtn);
+
                 this.list.innerHTML = "";
-                
+
+                //deleteItem function 
+                delBtn.addEventListener('click', (e) => {
+                   
+                });
+                //completeItem function
+                checkBtn.addEventListener('click', (e) => {
+                  li.classList.add('checked');
+                });
+
             }
 
-            this.list.value =" ";
+            this.list.value = " ";
 
         })
     }
-
-    deleteItem = function () { 
-       
-        
-
-       /*this.bin.addEventListener('click', (e) => {
-            let target = e.target;
-            target.classList.remove('fadeInOpacity');
-            target.classList.add('fadeOutOpacity');
-
-            setInterval(() => {
-                target.innerHTML="";
-                target.classList.remove('details');
-            }, 1000);
-
-        })*/
-
-    }
-
-    completedItem = function () {
-        let text=document.querySelector(".text");
-         text.addEventListener('click', (e) => {
-            let target = e.target;
-            target.classList.add('checked');
-        })
-        
-    }
-
-   
 
 }
 
-
-
-
 let newPush = new List();
 console.log(newPush.addItems());
-console.log(newPush.deleteItem());
+
+
+
 
 
 
