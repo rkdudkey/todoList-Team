ace8d30895cbc3f941c0a4e99f30b7b99182c307
rattikarn dudley <rattikarndudley@gmail.com>
2021-06-13 11:41:09 -0400

    soy version

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 
 class List {
-    list = document.querySelector("#list");
     add = document.querySelector('#btn');
-    warning = document.querySelector('.warning');
     ul = document.querySelector('#addList');
+    clearBtn = document.createElement('button');
     items = [];
 
     addItems = function () {
         this.add.addEventListener('click', (e) => {
             e.preventDefault();
 
-            let delBtn = document.createElement('button');
-            let checkBtn = document.createElement('button');
-            let li = document.createElement('li');
-
-            if (this.list.value === '') {
-                this.warning.innerHTML = "Please enter your to do list";
-                this.warning.style.color = "red";
+            let list = document.querySelector("#list");
+            let warning = document.querySelector('.warning');
+            
+            if (list.value === '') {
+                warning.innerHTML = "Please enter your to do list";
+                warning.style.color = "red";
             } else {
-                this.warning.innerHTML = "";
-                this.items.push(this.list.value);
+                
+                let delBtn = document.createElement('button');
+                let checkBtn = document.createElement('button');
+                let li = document.createElement('li');
+
+                warning.innerHTML = "";
+                this.items.push(list.value);
 
                 li.className = "details"
                 li.classList.add('fadeInOpacity');
@@ -27,25 +30,53 @@ class List {
                 checkBtn.innerHTML = `<i class="far fa-check-circle"></i>`;
                 li.innerHTML = `<span class="text">${this.items[this.items.length - 1]} </span>`;
 
+                //add styling classes both delBtn and checkedBtn
+                delBtn.classList.add('btn-del');
+                checkBtn.classList.add('btn-del');
+
                 //append the text and icons
                 this.ul.appendChild(li);
                 li.appendChild(delBtn);
                 li.appendChild(checkBtn);
 
-                this.list.innerHTML = "";
+                list.innerHTML = "";
 
+                console.log(this.ul.childElementCount);
+                
                 //deleteItem function 
                 delBtn.addEventListener('click', (e) => {
+                   let parent = delBtn.parentNode;
+
+                   // if ul has a child left at 2 elements
+                   //delete li and clear button
+                    if(this.ul.childElementCount === 2){
+                        parent.remove();
+                        this.clearBtn.remove();
+                    }else {
+                        parent.remove();
+                    }
                    
                 });
+
+
                 //completeItem function
                 checkBtn.addEventListener('click', (e) => {
                   li.classList.add('checked');
                 });
 
+
+                //clear all button
+                this.clearBtn.innerHTML = "Clear All"
+                this.ul.appendChild(this.clearBtn);
+                this.clearBtn.classList.add('clearBtn');
+                this.clearBtn.addEventListener('click', (e) => {
+                    this.ul.remove();
+                    window.location.reload();
+                })
+
             }
 
-            this.list.value = " ";
+            list.value = '';
 
         })
     }
@@ -54,6 +85,7 @@ class List {
 
 let newPush = new List();
 console.log(newPush.addItems());
+
 
 
 
